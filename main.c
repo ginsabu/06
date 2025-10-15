af22a684bcbb8487dd505098e8a15f49f3894dcf
ginsabu <155948685+ginsabu@users.noreply.github.com>
2025-10-15 21:24:24 +0900

    pr3.1

--- main.c
+++ main.c
@@ -3,17 +3,44 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-void func(void)
+int sumTwo(int a, int b)
 {
-	int x;
-	printf("func x is at %p\n", &x);
+    return a + b;
+}
+
+int square(int n)
+{
+    return n * n;
+}
+
+void get_max(int x, int y)
+{
+    if (x < y) {
+        printf("%d is bigger than %d\n", y, x);
+    } else if (x > y) {
+        printf("%d is bigger than %d\n", x, y);
+    } else {
+        printf("%d and %d have same value\n", x, y);
+    }
 }
 
 int main(void)
 {
-	int x;
-	printf("main x is at %p\n", &x);
-	func();
-	
-	return 0;
+    int a, b;
+    int n;
+    int x, y;
+
+    printf("Input two integers to sum: ");
+    scanf("%d %d", &a, &b);
+    printf("Sum is: %d\n", sumTwo(a, b));
+
+    printf("Input an integer to square: ");
+    scanf("%d", &n);
+    printf("Square is: %d\n", square(n));
+
+    printf("Input two integers to compare: ");
+    scanf("%d %d", &x, &y);
+    get_max(x, y);
+
+    return 0;
 }
