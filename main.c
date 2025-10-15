3263de4ca5da70a9fdc464c0f6fa2790ab3b09fe
ginsabu <155948685+ginsabu@users.noreply.github.com>
2025-10-15 22:44:51 +0900

    pr5

--- main.c
+++ main.c
@@ -3,14 +3,55 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int square(int a)
+int factorial(int n)
 {
-	return (a * a);
+	int res = 1;
+	
+	for (int i = 1; i <= n; i++) {
+		res *= i;
+	}
+	
+	return res;
 }
 
-int main()
+int combination(int n, int r)
 {
-	int a = 2;
-	a = square(a);
-	printf("a = %i\n", a);
+	int n_factorial;
+	int r_factorial;
+	int n_minus_r;
+	
+	n_factorial = factorial(n);
+	r_factorial = factorial(r);
+	n_minus_r = factorial(n-r);
+	
+	
+	return n_factorial / (n_minus_r * r_factorial);
+}
+
+int get_integer()
+{
+	int a;
+	
+	scanf("%d", &a);
+	
+	return a;
+}
+
+int main(void)
+{
+	int n;
+	int r;
+	int result;
+	
+	printf("Input n: ");
+	n = get_integer();
+	
+	printf("Input r: ");
+	r = get_integer();
+	
+	result = combination(n, r);
+	
+	printf("The combination value is: %d", result);
+	
+	return 0;
 }
